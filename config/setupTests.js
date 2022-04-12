47f1b1e3256cad6be7d2c44fa2bbb3499be1d79a
Zein Sleiman <zsleiman@zsleiman.remote.csb>
2022-04-12 08:33:14 -0500

    add config folder contents

---
+++ config/setupTests.js
@@ -0,0 +1,48 @@
+import { configure, mount, render, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react';
+
+configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.React = React;
+
+/**
+ * setup ENV vars
+ */
+process.env.BASE_PATH = '/api';
+
+export const entitlementsMock = {
+  bundle1: {
+    is_entitled: false,
+    is_trial: false,
+  },
+  bundle2: {
+    is_entitled: true,
+    is_trial: false,
+  },
+};
+
+export const getUserMock = {
+  entitlements: { ...entitlementsMock },
+  identity: { user: { is_org_admin: true } },
+};
+
+// mock insights instance
+global.insights = {
+  chrome: {
+    auth: {
+      getUser: () => new Promise((res) => res(getUserMock)),
+    },
+    appNavClick: () => Promise.resolve(),
+    getUserPermissions: () => Promise.resolve([]),
+    isBeta: () => true,
+    getEnvironment: () => 'test',
+    appAction: () => {},
+    appObjectId: () => {},
+  },
+};
+
+Element.prototype.scrollTo = () => {};
