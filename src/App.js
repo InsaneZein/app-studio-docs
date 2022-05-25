a117596942baa3b9db37ae41e360265c57299f87
Zein Sleiman <zsleiman@redhat.com>
2022-05-25 15:41:07 -0500

    fix linting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { Routes } from './Routes';
 import { MDXProvider } from '@mdx-js/react';
 import { Text, TextVariants, List, ListItem, TextContent, ListComponent } from '@patternfly/react-core';
-import { Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
+import { Thead, Tr, Tbody, Td } from '@patternfly/react-table';
 import './App.scss';
 
 const App = () => {
@@ -20,86 +20,87 @@ const App = () => {
   }, []);
 
   const components = {
-    h1: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h1}/>
-      </TextContent>
-      )
-    },
-    h2: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h2}/>
-      </TextContent>
-      )
-    },
-    h3: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h3}/>
-      </TextContent>
-      )
-    },
-    h4: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h4}/>
-      </TextContent>
-      )
-    },
-    h5: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h5}/>
-      </TextContent>
-      )
-    },
-    h6: props => {
-      return (
-      <TextContent>
-        <Text {...props} component={TextVariants.h6}/>
-      </TextContent>
-      )
-    },
-    a: props => {
+    h1: (props) => {
       return (
         <TextContent>
-          <Text {...props} component={TextVariants.a}/>
+          <Text {...props} component={TextVariants.h1} />
         </TextContent>
-      )
+      );
     },
-    p: props => {
+    h2: (props) => {
       return (
         <TextContent>
-          <Text {...props} component={TextVariants.p}/>
+          <Text {...props} component={TextVariants.h2} />
         </TextContent>
-      )
+      );
     },
-    li: props => {
+    h3: (props) => {
       return (
-        <ListItem {...props}/>
-      )
+        <TextContent>
+          <Text {...props} component={TextVariants.h3} />
+        </TextContent>
+      );
     },
-    ol: props => {
+    h4: (props) => {
       return (
-        <List {...props} component={ListComponent.ol}/>
-      )
+        <TextContent>
+          <Text {...props} component={TextVariants.h4} />
+        </TextContent>
+      );
     },
-    thead: props => {
+    h5: (props) => {
       return (
-        <Thead {...props}>
-        </Thead>
-      )
-    }
+        <TextContent>
+          <Text {...props} component={TextVariants.h5} />
+        </TextContent>
+      );
+    },
+    h6: (props) => {
+      return (
+        <TextContent>
+          <Text {...props} component={TextVariants.h6} />
+        </TextContent>
+      );
+    },
+    a: (props) => {
+      return (
+        <TextContent>
+          <Text {...props} component={TextVariants.a} />
+        </TextContent>
+      );
+    },
+    p: (props) => {
+      return (
+        <TextContent>
+          <Text {...props} component={TextVariants.p} />
+        </TextContent>
+      );
+    },
+    li: (props) => {
+      return <ListItem {...props} />;
+    },
+    ol: (props) => {
+      return <List {...props} component={ListComponent.ol} />;
+    },
+    thead: (props) => {
+      return <Thead {...props}></Thead>;
+    },
+    tr: (props) => {
+      return <Tr {...props}></Tr>;
+    },
+    tbody: (props) => {
+      return <Tbody {...props}></Tbody>;
+    },
+    td: (props) => {
+      return <Td {...props}></Td>;
+    },
   };
-
 
   return (
     <MDXProvider components={components}>
       <Routes />;
     </MDXProvider>
-  )
+  );
 };
 
 export default App;
